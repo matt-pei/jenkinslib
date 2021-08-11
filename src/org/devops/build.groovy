557a618c56c763f87615c73b40892eeb630d5cfe
matt-pei <sipei@163.com>
2021-08-11 14:19:49 +0800

    fix

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -4,8 +4,15 @@ def Build(buildType,buildShell){
     def buildTools = ["maven":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
     println("当前构建工具为 ${buildType}")
     buildHome = tool buildTools[buildType]
-
-    sh "${buildHome}/bin${buildType} ${buildShell}" 
+    
+    if ("${buildType}" == "npm"){
+        sh """export NODE_HOME=${buildHome}
+              export PATH=\$NODE_HOME/bin:\$PATH
+              ${buildHome}/bin/${buildType} ${buildShell}"""
+    } else {
+        sh "${buildHome}/bin${buildType} ${buildShell}"
+    }
+ 
 }
 
 

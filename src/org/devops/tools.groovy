2953d5854976a04c5f109ad36b6d7caf956d4425
matt-pei <sipei@163.com>
2021-12-14 16:52:11 +0800

    :art: Modify the format

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -10,7 +10,8 @@ def PrintMes(value,color){
     color = ['red'  : "\033[40;31m >>>>>>>>${value}<<<<<<<<<< \033[0m",
              'blue' : "\033[47;34m ${value} \033[0m",
              'green' : "[1;32m >>>>>>>>${value}<<<<<<<< [m"
-             'green1' : "\033[40;32m >>>>>>>>${value}<<<<<<<< \033[0m" ]
+             'green1' : "\033[40;32m >>>>>>>>${value}<<<<<<<< \033[0m"
+             ]
     ansiColor('xterm'){
         println(color[color])
     }
